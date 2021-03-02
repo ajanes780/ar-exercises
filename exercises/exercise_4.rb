c618c9e2736b1a54be28d9c14a423bd6eab0b24d
ajanes780 <aaron.janes@gmail.com>
2021-03-02 03:42:03 +0000

    Complete Q4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(name: "surrey",annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+
+whistler = Store.create(name: "whistler",annual_revenue: 1900000, mens_apparel: true, womens_apparel: false )
+
+
+yaletown = Store.create(name: "yaletown",annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel:true)
+
+for i in  @mens_stores do 
+
+  puts(" the store name is: #{i.name} total revenue is : #{i.annual_revenue}")
+  puts "************************"
+end
+
