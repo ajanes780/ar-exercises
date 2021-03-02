b8d4482264f851e723abbf267fb1ee4d3510fcc8
ajanes780 <aaron.janes@gmail.com>
2021-03-02 03:23:53 +0000

    finish Q2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,28 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+burnaby = Store.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+
+richmond = Store.create(name: "richmond",annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+
+gastown = Store.create(name: "gastown",annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+
+p Store.count
+
+# end
+# # Your code goes below here ...
+# class Burnaby < stores
+#   @annual_revenue = 1260000
+#   @mens_apparel = false
+#   @womens_appare = true
+# end
+
+# class Burnaby < stores
+#   @annual_revenue = 190000
+#   @mens_apparel = true
+#   @womens_appare = false
+# end
+
