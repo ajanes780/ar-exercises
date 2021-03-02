af33c79e8448cb7661b4e60820cee592bc33d41a
Aaron Janes <Aaron.Janes@gmail.com>
2021-03-01 19:52:57 -0700

    finsih Q1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+burnaby = Store.create(name: "Burnaby",annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+p burnaby
+
+richmond = Store.create(name: "richmond",annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+p richmond
+
+gastown = Store.create(name: "gastown",annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+p gastown
+
